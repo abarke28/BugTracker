931de18f43024bd3c88508bd8284342c48f22c2b
Alex Barker <abarke28@gmail.com>
2020-05-14 10:10:07 -0700

    edit bug vm core statuses moved to IEnumerable<SelectListItem> with non-exclusive flags mvoed to bools

--- BugTracker/Models/ViewModels/EditBugVm.cs
+++ BugTracker/Models/ViewModels/EditBugVm.cs
@@ -11,7 +11,8 @@ namespace BugTracker.Models.ViewModels
         public Bug Bug { get; set; }
         public string ProjectName { get; set; }
         public IEnumerable<SelectListItem> CoreStatuses { get; set; }
-        public IEnumerable<SelectListItem> AttributeStatuses { get; set; }
+        public bool Assigned { get; set; }
+        public bool Reopened { get; set; }
 
         public EditBugVm()
         {
@@ -21,13 +22,6 @@ namespace BugTracker.Models.ViewModels
                 new SelectListItem {Text = BugStatus.Resolved.ToString(), Value = BugStatus.Resolved.ToString()},
                 new SelectListItem {Text = BugStatus.Closed.ToString(), Value = BugStatus.Closed.ToString()}
             };
-
-            AttributeStatuses = new List<SelectListItem>
-            {
-                new SelectListItem {Text = BugStatus.Assigned.ToString(), Value = BugStatus.Assigned.ToString()},
-                new SelectListItem {Text = BugStatus.Reopened.ToString(), Value = BugStatus.Reopened.ToString()}
-            };
-
         }
     }
 }
