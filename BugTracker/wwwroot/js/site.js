ae750280f53cea71d6427bb640524721d9044ea4
Alex Barker <abarke28@gmail.com>
2020-05-17 10:05:15 -0700

    Added some js for date handling

--- BugTracker/wwwroot/js/site.js
+++ BugTracker/wwwroot/js/site.js
@@ -2,3 +2,9 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+Date.prototype.addDays = function (days) {
+    var date = new Date(this.valueOf());
+    date.setDate(date.getDate() + days);
+    return date;
+}
