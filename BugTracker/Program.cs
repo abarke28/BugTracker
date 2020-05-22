1e6787680c3a068c41896d7f7259dabf6e21c5d5
Alex Barker <abarke28@gmail.com>
2020-05-21 21:02:53 -0700

    Adding startup logging

--- BugTracker/Program.cs
+++ BugTracker/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
@@ -13,7 +14,10 @@ namespace BugTracker
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+            var logger = host.Services.GetRequiredService<ILogger<Program>>();
+            logger.LogInformation("Application has started");
+            host.Run();
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
