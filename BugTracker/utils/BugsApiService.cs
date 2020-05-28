3e5a5eaa82f30f430ddb22511ae7f37583cc5ada
Alex Barker <abarke28@gmail.com>
2020-05-28 12:02:24 -0700

    Add BugsApi typed client

---
+++ BugTracker/utils/BugsApiService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace BugTracker.utils
+{
+    public class BugsApiService
+    {
+        public HttpClient Client { get; set; }
+
+        public BugsApiService(HttpClient client)
+        {
+            client.BaseAddress = new Uri(@"https://localhost:44313/api/bugs/");
+
+            Client = client;
+        }
+    }
+}
