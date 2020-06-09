175cd9b572293b050ce06534c7dae41210962bae
Alex Barker <abarke28@gmail.com>
2020-06-09 07:50:21 -0700

    Add SelectLists for bug severity

--- BugTracker/Models/ViewModels/EditBugVm.cs
+++ BugTracker/Models/ViewModels/EditBugVm.cs
@@ -11,6 +11,7 @@ namespace BugTracker.Models.ViewModels
         public Bug Bug { get; set; }
         public string ProjectName { get; set; }
         public IEnumerable<SelectListItem> CoreStatuses { get; set; }
+        public IEnumerable<SelectListItem> Severities { get; set; }
         public bool Assigned { get; set; }
         public bool Reopened { get; set; }
 
@@ -22,6 +23,11 @@ namespace BugTracker.Models.ViewModels
                 new SelectListItem {Text = BugStatus.Resolved.ToString(), Value = BugStatus.Resolved.ToString()},
                 new SelectListItem {Text = BugStatus.Closed.ToString(), Value = BugStatus.Closed.ToString()}
             };
+
+            Severities = new List<SelectListItem>
+            (
+                Enumerable.Range(0,11).Select(i=>new SelectListItem {Text=i.ToString(), Value = i.ToString() })
+            );
         }
     }
 }
--- BugTracker/Models/ViewModels/NewBugVm.cs
+++ BugTracker/Models/ViewModels/NewBugVm.cs
@@ -1,4 +1,5 @@
 ﻿using BugTracker.Models.Dtos;
+using Microsoft.AspNetCore.Mvc.Rendering;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,5 +11,14 @@ namespace BugTracker.Models.ViewModels
     {
         public BugDto Bug { get; set; }
         public string ProjectName { get; set; }
+        public IEnumerable<SelectListItem> Severities { get; set; }
+
+        public NewBugVm()
+        {
+            Severities = new List<SelectListItem>
+            (
+                Enumerable.Range(0, 11).Select(i => new SelectListItem { Text = i.ToString(), Value = i.ToString() })
+            );
+        }
     }
 }
