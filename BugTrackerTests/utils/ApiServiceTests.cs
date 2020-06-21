d8a5673c512deb669a90d6b75639a2b61a0aa4e2
Alex Barker <abarke28@gmail.com>
2020-06-20 22:25:59 -0700

    Add ApiServiceTests.cs

---
+++ BugTrackerTests/utils/ApiServiceTests.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace BugTrackerTests.utils
+{
+    public class ApiServiceTests
+    {
+        [Fact]
+        public void BugsApiService_WhenCalled_ReturnsAll()
+        {
+
+        }
+    }
+}
